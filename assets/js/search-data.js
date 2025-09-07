f4f004685a39bdc84e85877eeaee36b2fb2e5711
CHENyiru3 <125234164+CHENyiru3@users.noreply.github.com>
2025-09-07 04:26:56 +0000

    Deploying to gh-pages from @ CHENyiru3/CHENyiru3.github.io@7d3da4c5e4aaa24a2a3b58fd33fb15a779719d2a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,28 +23,14 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-life",
+          title: "life",
+          description: "Moments and experiences",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/life/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-a-post-that-can-be-cited",
+        },{id: "post-a-post-that-can-be-cited",
         
           title: "a post that can be cited",
         
@@ -66,11 +52,11 @@ ninja.data = [{
             window.location.href = "/blog/2023/tikzjax/";
           
         },
-      },{id: "post-a-post-with-bibliography",
+      },{id: "post-post-bibliography",
         
-          title: "a post with bibliography",
+          title: "Post Bibliography",
         
-        description: "an example of a blog post with bibliography",
+        description: "",
         section: "Posts",
         handler: () => {
           
