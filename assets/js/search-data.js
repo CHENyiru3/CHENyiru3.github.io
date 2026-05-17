fc0ac3fdbcf9dfc434daa9d072c670b2b4b469dc
CHENyiru3 <125234164+CHENyiru3@users.noreply.github.com>
2026-05-17 04:36:15 +0000

    Deploying to gh-pages from @ CHENyiru3/CHENyiru3.github.io@037a1b8ab67b940837f7741b73318d3dcd732110 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -267,6 +267,11 @@ ninja.data = [{
           description: "",
           section: "Research_radar",handler: () => {
               window.location.href = "/research-radar/2026-05-16/";
+            },},{id: "research_radar-research-radar-2026-05-17",
+          title: 'Research Radar — 2026-05-17',
+          description: "",
+          section: "Research_radar",handler: () => {
+              window.location.href = "/research-radar/2026-05-17/";
             },},{
         id: 'social-email',
         title: 'email',
